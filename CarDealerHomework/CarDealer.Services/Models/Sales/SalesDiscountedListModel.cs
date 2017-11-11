cf4445b59d94e6df3294fb78d11a9f6ac0ba2d57
IvVelichkova <night_princess.1986@abv.bg>
2017-11-11 15:09:44 +0200

    new views

---
+++ CarDealerHomework/CarDealer.Services/Models/Sales/SalesDiscountedListModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CarDealer.Services.Models.Cars;
+
+namespace CarDealer.Services.Models.Sales
+{
+    public class SalesDiscountedListModel 
+    {
+        public decimal Price { get; set; }
+
+        public double Discount { get; set; }
+
+        public int Id { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public CarModel Car { get; set; }
+
+    }
+}
---
+++ CarDealerHomework/CarDealer/Infrastructure/Extensions/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarDealer.Web.Infrastructure.Extensions
+{
+    public static class StringExtensions
+    {
+        private const string format = "F2";
+        public static string ToPrice (this decimal priceText)
+        {
+            return $"${priceText.ToString(format)}";
+        }
+
+        public static string ToPercentage (this double number)
+        {
+            return $"{number.ToString(format)}%";
+        }
+    }
+}
